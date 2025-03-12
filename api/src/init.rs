ee8e1da2b2c66e38fd89bd76e3d95724b65bce64
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2025-03-12 10:06:52 -0400

    changed vm-setup file and added server start logs

--- api/src/init.rs
+++ api/src/init.rs
@@ -15,21 +15,29 @@ use std::env;
 pub static mut DB: Option<Db> = None;
 
 pub async fn run() {
+    println!("Starting server...");
+
     dotenv::dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    println!("Database URL loaded from .env file");
+
     let pool = PgPoolOptions::new()
         .max_connections(20)
         .connect(&db_url)
         .await
         .unwrap();
     let db = Db::new(pool);
+    println!("Database connection established");
 
     let app = Router::new()
         .route(&fen_path("/login"), post(Login::handle))
         .route(&fen_path("/signup"), post(Signup::handle))
         .route(&fen_path("/get-repertoire"), get(GetRepertoire::handle))
         .layer(Extension(db));
+    println!("Routes registered");
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
+    println!("Listening on localhost:4000!\n");
+
     axum::serve(listener, app).await.unwrap();
 }
---
+++ vm-setup.sh
@@ -0,0 +1,133 @@
+# VM setup
+
+# create new user
+adduser kiahjh
+
+# grant sudo privileges to kiahjh
+usermod -aG sudo kiahjh
+
+ufw allow OpenSSH
+ufw enable
+
+# switch to kiahjh
+su kiahjh
+mkdir ~/.ssh
+
+rsync --archive --chown=kiahjh:kiahjh ~/.ssh /home/kiahjh # copy over authorized keys
+exit # exit back to root
+
+# don't ask kiahjh for sudo password:
+echo "kiahjh ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# install postgres
+su kiahjh
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+
+# switch to created postgres user
+sudo -i -u postgres
+createuser kiahjh
+
+# open psql shell for a hot second and then
+ALTER USER kiahjh WITH SUPERUSER;
+\q; # exit psql shell
+
+exit # exit back to kiahjh
+createdb replist # ... in order to create the database as kiahjh
+
+# then, inside psql,
+GRANT ALL PRIVILEGES ON DATABASE replist TO kiahjh;
+\password kiahjh # set password for kiahjh
+
+# generate an ssh key to access github
+su kiahjh
+ssh-keygen -t rsa -C "miciahjohnhenderson@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+# set timezone (for cron jobs and stuff)
+sudo timedatectl set-timezone America/New_York
+
+# allocate extra swap space (so we don't run out of memory when compiling tokio)
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+sudo sysctl vm.vfs_cache_pressure=50
+echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
+echo "vm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf
+
+# clone repo
+git clone git@github.com:kiahjh/RepList.git
+
+# install Rust and Cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/env # add cargo to path
+sudo apt install build-essential # install gcc toolchain
+
+# edit ~/Replist/api/.env
+# DATABASE_URL=postgres://kiahjh:password@localhost/replist
+
+# seed db
+psql -U kiahjh -d replist -f ~/RepList/api/migrations/00_init/up.sql
+
+# build api
+cd ~/RepList/api
+cargo build --release # takes a hot minute
+
+# install and configure nginx
+sudo apt install nginx
+sudo vim /etc/nginx/sites-available/api.replist.innocencelabs.com.conf
+
+# paste in the following:
+# -------
+server {
+    listen 80;
+    server_name api.replist.innocencelabs.com;
+
+    location / {
+        proxy_pass http://localhost:4000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+# -------
+
+sudo ln -s /etc/nginx/sites-available/api.replist.innocencelabs.com.conf /etc/nginx/sites-enabled/
+sudo nginx -t # test nginx config
+sudo systemctl restart nginx
+
+# set up daemon with systemctl
+sudo vim /etc/systemd/system/replist-api.service
+
+# paste in the following:
+# -------
+[Unit]
+Description=RepList API
+After=network.target
+
+[Service]
+ExecStart=/home/kiahjh/RepList/api/target/release/api
+WorkingDirectory=/home/kiahjh/RepList/api
+Restart=always
+User=root
+Environment=DATABASE_URL=postgres://kiahjh:password@localhost/replist
+
+[Install]
+WantedBy=multi-user.target
+# -------
+
+sudo systemctl daemon-reload
+sudo systemctl start replist-api.service
+sudo systemctl enable replist-api.service
+sudo systemctl status replist-api.service # check status
+# manage it with stop/start/restart/status
+
+# install certbot
+sudo apt update
+sudo apt install certbot python3-certbot-nginx
+sudo certbot --nginx -d api.replist.innocencelabs.com -d www.api.replist.innocencelabs.com
